19814f202075f5598986a34b8980cb88498c3bd1
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2024-06-02 18:31:36 +0600

    added employee pages

--- src/pages/Dashboard/Employee/PaymentHistory.jsx
+++ src/pages/Dashboard/Employee/PaymentHistory.jsx
@@ -2,6 +2,34 @@ const PaymentHistory = () => {
   return (
     <div>
       <h1>this is payment history</h1>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Month</th>
+                <th>Amount</th>
+                <th>Transaction Id</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr>
+              {/* row 2 */}
+              <tr>
+                <td>Hart Hagerty</td>
+                <td>Desktop Support Technician</td>
+                <td>Purple</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Dashboard/Employee/WorkSheet.jsx
+++ src/pages/Dashboard/Employee/WorkSheet.jsx
@@ -2,6 +2,74 @@ const WorkSheet = () => {
   return (
     <div>
       <h1>this is work sheet page</h1>
+      {/* table */}
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Task</th>
+                <th>Hours Worked</th>
+                <th>Date</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+                <td>Submitted</td>
+              </tr>
+            </tbody>
+          </table>
+          <form className="overflow-x-auto flex flex-row justify-between items-center gap-16 mt-10">
+            {/* task */}
+            <div className="mb-3 w-full">
+              <label className="form-control w-full">Task</label>
+
+              <select
+                name="task"
+                required
+                className="input input-bordered  block w-full px-4 py-2 mt-2"
+              >
+                <option>Sales</option>
+                <option>Support</option>
+                <option>Content</option>
+                <option>Paper-work</option>
+              </select>
+            </div>
+            {/* Hours Worked */}
+            <div className="mb-3 w-full">
+              <label className="form-control w-full">Hours Worked</label>
+              <input
+                type="number"
+                placeholder="Hours Worked"
+                name="hours_worked"
+                required
+                className="input input-bordered  block w-full px-4 py-2 mt-2"
+              />
+            </div>
+            {/* date */}
+            <div className="mb-3 w-full">
+              <label className="form-control w-full">Date</label>
+              <input
+                type="number"
+                placeholder="Hours Worked"
+                name="hours_worked"
+                required
+                className="input input-bordered  block w-full px-4 py-2 mt-2"
+              />
+            </div>
+            {/* btn */}
+            <div className="mt-6 w-full">
+              <button className="btn w-full ">Register</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
