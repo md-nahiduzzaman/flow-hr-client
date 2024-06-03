df746ac29e1275e04c72f037688eeab32a03ab04
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2024-06-03 20:10:35 +0600

    added get user for admin

--- src/pages/Dashboard/Admin/AllEmployeeList.jsx
+++ src/pages/Dashboard/Admin/AllEmployeeList.jsx
@@ -10,10 +10,78 @@ const AllEmployeeList = () => {
   } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
-      const { data } = await axios(`${import.meta.env.VITE_API_URL}/users`);
+      const { data } = await axios(
+        `${import.meta.env.VITE_API_URL}/users?verified=true`
+      );
       return data;
     },
   });
+
+  // salary change
+  const handleSalarySubmit = async (e, id, oldSalary) => {
+    e.preventDefault();
+    const form = e.target;
+    const salary = form.newSalary.value;
+    const salaryData = {
+      salary,
+    };
+    console.log(id);
+    console.log(salaryData);
+
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_API_URL}/user-salary/${id}`,
+        salaryData
+      );
+      console.log("Salary updated:", response.data);
+      refetch();
+    } catch (err) {
+      console.log(err);
+    }
+    // close modal
+    // const modal = document.getElementById(`my_modal_${id}`);
+    // modal.close();
+  };
+
+  // role change
+  const handleMakeHR = async (id) => {
+    const role = "HR";
+    const roleData = {
+      role,
+    };
+
+    console.log(roleData, id);
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_API_URL}/user-role/${id}`,
+        roleData
+      );
+      console.log("Role updated:", response.data);
+      refetch();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // fire
+  const handleMakeFire = async (id) => {
+    const status = "fired";
+    const roleData = {
+      status,
+    };
+
+    console.log(roleData, id);
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_API_URL}/user-status/${id}`,
+        roleData
+      );
+      console.log("Status updated:", response.data);
+      refetch();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   console.log(users);
   if (isLoading) return <LoadingSpinner></LoadingSpinner>;
@@ -29,6 +97,8 @@ const AllEmployeeList = () => {
               <tr>
                 <th>Name</th>
                 <th>Designation</th>
+                <th>Status</th>
+                <th>Salary</th>
                 <th>Make HR</th>
                 <th>Fire</th>
               </tr>
@@ -38,13 +108,94 @@ const AllEmployeeList = () => {
                 <tr key={user._id}>
                   <td>{user?.name}</td>
                   <td>{user?.designation}</td>
+                  <td>{user?.verified ? "Verified" : "Not Verified"}</td>
                   <td>
-                    <button className="btn btn-ghost btn-xs">Make HR</button>
+                    <button className="btn btn-ghost btn-xs">
+                      {user?.salary}
+                    </button>
+                    {/* modal */}
+                    {/* Open the modal using document.getElementById('ID').showModal() method */}
+                    <button
+                      className="btn btn-xs"
+                      onClick={() =>
+                        document
+                          .getElementById(`my_modal_${user?._id}`)
+                          .showModal()
+                      }
+                    >
+                      Change salary
+                    </button>
+                    <dialog id={`my_modal_${user?._id}`} className="modal">
+                      <div className="modal-box flex flex-col items-center">
+                        <h3 className="font-bold text-lg">{user?.name}</h3>
+                        <p className="py-4">Present Salary: {user?.salary}</p>
+                        <div className="modal-action">
+                          <form
+                            onSubmit={(e) =>
+                              handleSalarySubmit(e, user?._id, user?.salary)
+                            }
+                            method="dialog"
+                          >
+                            <div className="col-span-2 w-full">
+                              <label className="form-control w-full">
+                                <div className="label">
+                                  <span className="label-text">New Salary</span>
+                                </div>
+                                <input
+                                  type="number"
+                                  name="newSalary"
+                                  className="input input-bordered w-full"
+                                  required
+                                />
+                              </label>
+                            </div>
+                            {/* if there is a button in form, it will close the modal */}
+                            <button
+                              onClick={() => document.closeModal()}
+                              className="btn mt-5 w-full"
+                            >
+                              Submit
+                            </button>
+                          </form>
+                        </div>
+                      </div>
+                    </dialog>
                     {/* HR */}
                   </td>
                   <td>
-                    <button className="btn btn-ghost btn-xs">Fire</button>
-                    {/* Fired */}
+                    {user?.role === "HR" ? (
+                      "HR"
+                    ) : (
+                      <button
+                        onClick={() => handleMakeHR(user._id)}
+                        className="btn btn-ghost btn-xs"
+                        hidden={user?.role === "HR"}
+                      >
+                        Make HR
+                      </button>
+                    )}
+
+                    {/* <button
+                      onClick={() => handleMakeHR(user._id)}
+                      className="btn btn-ghost btn-xs"
+                      disabled={user?.role === "HR"}
+                    >
+                      {user?.role === "HR" ? "HR" : "Make HR"}
+                    </button> */}
+                    {/* HR */}
+                  </td>
+                  <td>
+                    {user?.status === "fired" ? (
+                      "Fired"
+                    ) : (
+                      <button
+                        onClick={() => handleMakeFire(user._id)}
+                        className="btn btn-ghost btn-xs"
+                        hidden={user?.status === "working"}
+                      >
+                        Fire
+                      </button>
+                    )}
                   </td>
                 </tr>
               ))}
